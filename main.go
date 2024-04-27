7ff6472ab7ad427d524154ccc3fd0d15402a95f1
shreyash2101 <chamaria.21.shreyash@gmail.com>
2024-04-27 16:37:01 +0530

    final setup and backend port setup

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/shreyash2101/online-job-portal/router"
 )
 
 func main() {
 	fmt.Println("Welcome to online job portal")
-
+	
+	r := router.Router()
+	fmt.Println("Server is getting started at port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
